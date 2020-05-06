337edca435b7fe8a9648217ceb516b36f0071b19
Santiago Ruiz <santiago.ruiz@bewe.io>
2020-05-06 17:26:02 -0500

    documents view

---
+++ resources/views/documento/listar.blade.php
@@ -0,0 +1,127 @@
+@section('content')
+<div class="juridico right-buttons">
+    <div>
+        @isset ($permissions->crear)
+        <a href="javascript:void(0)" onclick="documento.createEditModal()" class="btn btn-default">
+            Crear
+        </a>
+        @endisset
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 flex juridico" id="filter-form-container">
+        <div class="pull-left flex" style="padding-right:20px"></div>
+    </div>
+</div>
+
+<table id="tipoProcesoTable" class="table table-hover" data-empty="Sin documentos"
+    data-paging-count-format="Mostrando del {PF} al {PL} de {TR} registros"
+    data-filter-container="#filter-form-container" data-sorting="true" data-filtering="true" data-paging="true"
+    data-filter-placeholder="Buscar ..." data-filter-position="left" data-filter-dropdown-title="Buscar por"
+    data-filter-space="OR">
+    <thead>
+        <tr class="bg-success">
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>¿obligatorio?</th>
+            <th>Estado</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @if (count($documentos) > 0)
+        @foreach ($documentos as $documento)
+        <tr id="tipoProcesoRow{{$documento['id_documento']}}">
+            <td>{{$documento['id_documento']}}</td>
+            <td>{{$documento['nombre_documento']}}</td>
+            <td>{{$documento['obligatoriedad_documento'] == 1 ? 'Sí' : 'No'}}</td>
+            <td>{{$documento['estado_documento'] == 2 ? 'Inactivo' : 'Activo'}}</td>
+            <td>
+                <div class="flex justify-center table-actions">
+                    @isset ($permissions->editar)
+                    <a href="javascript:void(0)"
+                        onclick="documento.createEditModal('{{$documento['id_documento']}}')"
+                        class="btn text-primary" type="button">
+                        <span class="glyphicon glyphicon-pencil"></span>
+                    </a>
+                    @endisset
+                    @isset ($permissions->eliminar)
+                    <a href="javascript:void(0)" class="btn text-danger" type="button"
+                        onclick="documento.openDelete('{{$documento['id_documento']}}')">
+                        <span class="glyphicon glyphicon-remove"></span>
+                    </a>
+                    @endisset
+                </div>
+            </td>
+        </tr>
+        @endforeach
+        @endif
+    </tbody>
+</table>
+
+@isset ($permissions->eliminar)
+<div class="modal fade" tabindex="-1" role="dialog" id="deleteModal">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Eliminar documento</h4>
+            </div>
+            <div class="modal-body">
+                <p>¿Está seguro que desea eliminar el documento?</p>
+            </div>
+            <div class="modal-footer center">
+                <input type="hidden" id="deleteValue" />
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <button type="button" onClick="documento.delete()" class="btn btn-danger">Eliminar</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endisset
+
+@if(isset($permissions->crear) || isset($permissions->editar))
+<div class="modal fade" tabindex="-1" role="dialog" id="createModal">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" id="createTitle"></h4>
+            </div>
+            <form onsubmit="documento.upsert(event)">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="recipient-name" class="control-label">Nombre del documento</label>
+                        <input type="text" class="form-control required" id="documentoNombre" name="nombre_documento">
+                    </div>
+                    <div class="form-group">
+                        <label for="recipient-name" class="control-label">¿Es obligatorio?</label>
+                        <div class="checkbox-form">
+                            <input type="checkbox" data-on="Sí" data-off="No" data-width="90"
+                                class="form-control" id="documentoObligatorio" name="obligatorio" checked />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="recipient-name" class="control-label">Estado</label>
+                        <div class="checkbox-form">
+                            <input type="checkbox" data-on="Activo" data-off="Inactivo" data-width="90"
+                                class="form-control" id="documentoEstado" name="estado" checked />
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer center">
+                    <input type="hidden" id="createValue" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-success">Guardar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endif
+
+
+@endsection
